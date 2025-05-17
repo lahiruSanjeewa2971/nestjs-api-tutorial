72a0a2a4196efb32e0e6e2ed77f6a83bf36cb0cc
lahiruSanjeewa2971 <rathne1997@gmail.com>
2025-05-18 01:09:50 +0530

    Added ORM tool prisma to connect with postgre SQL db which is inside of a docker image.

---
+++ prisma/migrations/20250517192640_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "in" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("in")
+);
+
+-- CreateTable
+CREATE TABLE "Bookmark" (
+    "in" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "link" TEXT NOT NULL,
+
+    CONSTRAINT "Bookmark_pkey" PRIMARY KEY ("in")
+);
